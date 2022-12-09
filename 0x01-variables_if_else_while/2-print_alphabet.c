d838ec207bd6574a63163f1a510fe792fc2d875a
Peter Kefa Mjomba <petermjomba@gmail.com>
2022-12-09 10:36:16 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints alphabets in lowercase
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
