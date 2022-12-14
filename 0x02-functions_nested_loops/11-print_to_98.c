adbe13955c189f5e6701c14a4366c6c415e8b757
Peter Kefa Mjomba <petermjomba@gmail.com>
2022-12-14 11:47:55 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,17 +13,17 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++;)
+		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-				printf("%d, ", i)
+				printf("%d, ", i);
 			else if (i == 98)
-				printf("%d\n", i)
+				printf("%d\n", i);
 		}
 	}
 	else if (n >= 98)
 	{
-		for (j = n; j >= 98; j--;)
+		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
 				printf("%d, ", j);
