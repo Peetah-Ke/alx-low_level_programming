4b598dfc71773c1b40fda95268de526654dc3a1b
Peter Kefa Mjomba <petermjomba@gmail.com>
2022-12-19 11:48:37 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,47 +1,35 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
  */
 
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-		i = 0;
-		d = 0;
-		n = 0;
-		len = 0;
-		f = 0;
-		digit = 0;
-
-			while (s[len] != '\0')
-				len++;
-
-			while (i < len && f == 0)
+		while (*(s + count) != '\0')
 		{
-				if (s[i] == '-')
-					++d;
-					if (s[i] >= '0' && s[i] <= '9')
+			if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+					break;
+			if (*(s + count) == '-')
+					pn *= -1;
+
+			if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+			{
+					if (size > 0)
+						m *= 10;
+					size++;
+			}
+			count++;
+		}
+
+		for (i = count - size; i < count; i++)
 		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-
-				break;
-
-			f = 0;
+			oi = oi + ((*(s + i) - 48) * m);
+			m /= 10;
 		}
-					i++;
-		}
-			if (f == 0)
-
-				return (0);
-				return (n);
+		return (oi * pn);
 }
