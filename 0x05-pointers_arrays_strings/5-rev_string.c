a1227abeceb7f6bce7fe19bfb4602c7b30178e03
Peter Kefa Mjomba <petermjomba@gmail.com>
2022-12-19 10:45:57 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,14 +12,13 @@ void rev_string(char *s)
 		int counter = 0;
 		int i;
 
-			while (s[counter] != '\0')
-				counter++;
+				while (s[counter] != '\0')
+					counter++;
 				for (i = 0; i < counter; i++)
-		{
-				counter--;
-				rev = s[i];
-				s[i] = s[counter];
-				s[counter] = rev;
-		}
+			{
+					counter--;
+					rev = s[i];
+					s[i] = s[counter];
+					s[counter] = rev;
+			}
 }
-
