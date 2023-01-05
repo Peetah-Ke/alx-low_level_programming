68e6b37012f0b3c98289bf4f5aa3126837f8629d
Peter Kefa Mjomba <petermjomba@gmail.com>
2023-01-05 06:42:48 -0800

    _putchar file update

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -6,7 +6,7 @@
  * @c: The character to print
  *
  * Return: On Success 1
- * On error: -1 is returned and errno is set appropriately
+ * On error, -1 is returned and errno is set appropriately
  */
 
 int _putchar(char c)
