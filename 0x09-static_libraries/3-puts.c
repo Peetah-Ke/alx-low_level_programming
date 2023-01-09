38dbe305707df8868221e30d6c1a1c39f5a3a197
Peter Kefa Mjomba <petermjomba@gmail.com>
2023-01-09 06:39:16 -0800

    second batch of .c files

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+void _puts(char *str)
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * _puts: describs puts function
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
