17eab97b75f86f30e32e42880aa6d25fc706ffea
Peter Kefa Mjomba <petermjomba@gmail.com>
2023-01-09 13:03:38 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,9 +3,7 @@
 
 /**
  * _atoi - converts a string to an integer
- *
  * @s: string to be converted
- *
  * Return: the int converted from the string
  *
  */
@@ -16,11 +14,17 @@ int _atoi(char *s)
 	int i, d, n, len, f, digit;
 
 	i = 0;
+
 	d = 0;
+
 	n = 0;
+
 	len = 0;
+
 	f = 0;
+
 	digit = 0;
+
 	while (s[len] != '\0')
 
 	len++;
@@ -29,13 +33,16 @@ int _atoi(char *s)
 	{
 		if (s[i] == '-')
 		++d;
+		
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
+
 			if (d % 2)
 				digit = -digit;
-				n = n * 10 + digit;
-				f = 1;
+
+			n = n * 10 + digit;
+			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
