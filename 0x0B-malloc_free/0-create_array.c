68d9f4748eb0656da21c81b5f7c6a04ba84614e7
Peter Kefa Mjomba <petermjomba@gmail.com>
2023-01-11 10:01:13 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -21,10 +21,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	str = malloc(sizeof(char) * size);
-
 	if (size == 0 || str == NULL)
 		return (NULL);
-		for (i = 0; i < size; i++)
-			str[i] = c;
-			return (str);
+
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
 }
