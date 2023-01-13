cacf6af97fe9e040eb292a19addec3c1df45eca0
Peter Kefa Mjomba <petermjomba@gmail.com>
2023-01-13 06:20:24 -0800

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,22 +24,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len1++;
 	while (s2 && s2[len2])
 		len2++;
+
 	if (n < len2)
 		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
 	if (!s)
 		return (NULL);
+
 	while (i < len1)
 	{
 		s[i] = s1[i];
 		i++;
 	}
+
 	while (n < len2 && i < (len1 + n))
 		s[i++] = s2[j++];
+
 	while (n >= len2 && i < (len1 + len2))
 		s[i++] = s2[j++];
 		s[i] = '\0';
-	return (s);
+		return (s);
 
 }
